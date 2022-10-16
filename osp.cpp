45cdded9bc10ed96b3378c2b3e352a46e13d5f5a
Tu Weijun <1184409092@qq.com>
2022-10-16 15:27:41 +0800

    完成了isp和osp函数

--- osp.cpp
+++ osp.cpp
@@ -7,7 +7,41 @@ osp::osp()
 
 int osp::get(char ch)
 {
-    //请编写
-    return 1;//占位符
+    //Judge the incoming characters and return the off stack priority
+        switch (ch) {
+        case '=':
+            return 0;
+            break;
+        case '+':
+            return 2;
+            break;
+        case '-':
+            return 2;
+            break;
+        case '*':
+            return 4;
+            break;
+        case '/':
+            return 4;
+            break;
+        case '%':
+            return 4;
+            break;
+        case '(':
+            return 8;
+            break;
+        case ')':
+            return 1;
+            break;
+        case '^':
+            return 6;
+            break;
+        case '&':
+            return 6;
+            break;
+        default:
+            break;
+        }
+        return 1;//占位符
 
 }
