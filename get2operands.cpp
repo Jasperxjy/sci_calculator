5cdb763b68405d2f21d5e490d5941f6e9f8a8c54
Xiao YuanH <2693670736@qq.com>
2022-10-16 17:58:46 +0800

    完成get2operands（）取栈前两位数

--- get2operands.cpp
+++ get2operands.cpp
@@ -7,7 +7,19 @@ get2operands::get2operands()
 
 bool get2operands::get(MyLStack<double> &opend, double &x, double &y)
 {
+
+    if(opend.length()>=2){
+        //若为正常情况，站外低于站内时 一定有大于两个操作数
+        x=opend.pop();
+        y=opend.pop();
+
+    }else{
+        //若操作数小于两个，说明出现了符号过多的错误
+
+        return false;
+    }
+
     //请编写
-    return false;//占位符
+    return true;//占位符
 }
 
